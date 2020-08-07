80a8b855cf8cc20af417dcea4e1e99f4b8ccc137
murph <murphyl@outlook.com>
2020-08-07 16:37:56 +0800

    fix function error

--- api/test.js
+++ api/test.js
@@ -1,6 +1,35 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync');
-// const db = low(new FileSync('blog.x.json'));
+const path = require('path');
+const low = require('lowdb');
+const fs = require('graceful-fs');
+const Base = require('lowdb/adapters/Base');
+
+const readFile = fs.readFileSync
+const writeFile = fs.writeFileSync
+
+class FileSync extends Base {
+  read() {
+    if (fs.existsSync(this.source)) {
+      try {
+        const data = readFile(this.source, 'utf-8').trim()
+        // Handle blank file
+        return data ? this.deserialize(data) : this.defaultValue
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          e.message = `Malformed JSON in file: ${this.source}\n${e.message}`
+        }
+        throw e
+      }
+    } else {
+      return this.defaultValue
+    }
+  }
+
+  write(data) {
+    return true;
+  }
+}
+
+const db = low(new FileSync(path.join(process.cwd(), 'blog.x.json')));
 
 export default (req, res) => {
 	const posts = [] //db.defaults({ blog: [] }).get('blog').value()

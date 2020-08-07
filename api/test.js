e6e05e30f6105bffb690a2d4ae83dabb762af6de
murph <murphyl@outlook.com>
2020-08-07 16:28:43 +0800

    fix function error

--- api/test.js
+++ api/test.js
@@ -1,8 +1,8 @@
 const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const db = low(new FileSync('blog.x.json'));
+const FileSync = require('lowdb/adapters/FileSync');
+// const db = low(new FileSync('blog.x.json'));
 
 export default (req, res) => {
-	const posts = db.defaults({ blog: [] }).get('blog').value()
-	res.json({ name: 'murph', email: 'murphyl@outlook.com', posts })
+	// const posts = db.defaults({ blog: [] }).get('blog').value()
+	res.json({ name: 'murph', email: 'murphyl@outlook.com', posts, path: __dirname })
 }

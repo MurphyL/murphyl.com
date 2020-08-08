590902d8b5ec20dcf54f74a387c36d57d81fb8e7
murph <murphyl@outlook.com>
2020-08-08 09:12:15 +0800

    serverless functions

--- api/test.js
+++ api/test.js
@@ -1,52 +1,35 @@
-const path = require('path');
 const low = require('lowdb');
-const fs = require('graceful-fs');
-const Base = require('lowdb/adapters/Base');
-
-const readFile = fs.readFileSync;
+const axios = require('axios');
+const Memory = require('lowdb/adapters/Memory');
 
 
+const local = {};
 
-class FileSync extends Base {
-  read() {
-    if (fs.existsSync(this.source)) {
-      try {
-        const data = readFile(this.source, 'utf-8').trim()
-        return data ? this.deserialize(data) : this.defaultValue
-      } catch (e) {
-        if (e instanceof SyntaxError) {
-          e.message = `Malformed JSON in file: ${this.source}\n${e.message}`
-        }
-        throw e
-      }
-    } else {
-      return this.defaultValue
-    }
-  }
-
-  write(data) {
-    return true;
-  }
+const send = (resp) => {
+	resp.json({
+		code: -1,
+		message: "truning"
+	})
 }
 
-const db = low(new FileSync(path.join(process.cwd(), 'blog.x.json')));
-
 export default (req, res) => {
-	console.log(__dirname, fs.readdirSync(__dirname));
-	console.log('----------------------------------------');
-	console.log(path.resolve('../'), fs.readdirSync(path.resolve('../')));
-	console.log('----------------------------------------');
-	console.log(process.cwd(), fs.readdirSync(process.cwd()));
-	console.log('----------------------------------------');
-	console.log('blog.x.json', fs.existsSync('blog.x.json'));
-	console.log('../blog.x.json', fs.existsSync('../blog.x.json'));
-	console.log('/ABS/blog.x.json', fs.existsSync(path.join(process.cwd(), 'blog.x.json')));
-	const posts = db.defaults({ blog: [] }).get('blog').value();
-	res.json({ 
-		name: 'murph', 
-		email: 'murphyl@outlook.com', 
-		posts, 
-		path: __dirname,
-		l: process.cwd()
-	})
+	const unique = req.query.db;
+
+	if(!unique) {
+		res.json({
+			code: 1,
+			message: '未指定数据库'
+		});
+		return;
+	}
+	if(local[unique]) {
+		send(res);
+	} else {
+		axios.get(unique).then((data) => {
+			local[unique] = low(new FileSync({}));
+		}).then((data) => {
+			send(res);
+		})
+	}
+
 }

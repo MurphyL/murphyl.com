fc385cdc2f1133ad80930514a752bb218cac141e
murph <murphyl@outlook.com>
2020-08-26 13:26:01 +0800

    联系函数

--- api/contact.js
+++ api/contact.js
@@ -3,13 +3,25 @@ const axios = require('axios');
 const slackWebhook = `https://hooks.slack.com/services/${process.env.SLACK_WEBHOOK}`
 
 export default async (req, res) => {
+
 	const { message = 'hello, murph' } = req.query;
-	const invokeResp = { desc: 'Slack发送消息到群组' };
-	try{
-		const slack = await axios.post(slackWebhook, { text: message });
-		Object.assign(invokeResp, { code: 0, playload: { slack } });
-	} catch(error) {
-		Object.assign(invokeResp, { code: 1, error: '发送消息错误' });
-	}
-	res.json(invokeResp);
+	
+	axios.post(slackWebhook, { text: message }).then(resp => {
+		console.log('发送消息成功', resp);
+		res.json({
+			code: 0,
+			payload: {
+				message,
+			}
+		});
+	}).catch(error => {
+		console.error('发送消息出错：', error);
+		res.json({
+			code: 1,
+			payload: {
+				message,
+			}
+		});
+	});
+
 };

b9ed8ed1e9c2326e8f70b63ceca6220af118784b
murph <murphyl@outlook.com>
2020-08-26 13:06:55 +0800

    联系函数

--- api/contact.js
+++ api/contact.js
@@ -2,22 +2,34 @@ const axios = require('axios');
 
 const slackWebhook = `https://hooks.slack.com/services/${process.env.SLACK_WEBHOOK}`
 
-export default async (req, res) => {
+export default (req, res) => {
 
 	const { message = 'hello, murph' } = req.query;
 	
-	const html = await axios.post(slackWebhook, {
-		params: { 'payload': JSON.stringify({ text: message }) }
-	});
-
-	console.log('Webhook url:', slackWebhook);
-	console.log('Response:', html);
-
-	res.json({
-		code: 0,
-		payload: {
-			message, html
+	axios.post(slackWebhook, {
+		params: { 
+			payload: { text: message } 
 		}
+	}).then(resp => {
+		console.log('Webhook url:', slackWebhook);
+		console.log('Response:', resp);
+		console.log('发送消息成功');
+		res.json({
+			code: 0,
+			payload: {
+				message,
+				slackWebhook
+			}
+		});
+	}).catch(error => {
+		console.log('发送消息出错~');
+		res.json({
+			code: 1,
+			payload: {
+				message,
+				slackWebhook
+			}
+		});
 	});
 
 };

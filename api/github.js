6f8af5b0768227afd32128c11fa9449e08dcd492
murph <murphyl@outlook.com>
2020-08-27 14:34:33 +0800

    改造API - v1.1

--- api/github.js
+++ api/github.js
@@ -1,23 +1,21 @@
 const axios = require('axios');
 const lodash = require('lodash');
 
-const githubWebhook = 'https://api.github.com/graphql';
-
-const githubToken = process.env.GITHUB_WEBHOOK;
+const { GITHUB_TOKEN } = process.env;
 const githubUser = process.env.NOW_GITHUB_ORG;
 const githubRepo = process.env.NOW_GITHUB_REPO;
 
 const githubConfig = {
 	headers:{
-		Authorization: `Bearer ${githubToken}`
+		Authorization: `Bearer ${GITHUB_TOKEN}`
 	}
 };
 
+const endpoint = 'https://api.github.com/graphql';
+
 const QUERY_REPO = `query{ repository(owner:"${githubUser}", name:"${githubRepo}") { id } }`;
 
-const createIssue = ({ repositoryId, title, body }) => (`mutation CreateIssuePayload { createIssue(input:{ repositoryId: "${ repositoryId }", title: "${ title }", body: "${ body }" }) { clientMutationId } }`);
-
-const repoFetched = axios.post(githubWebhook, { query: QUERY_REPO }, githubConfig);
+const repoFetched = axios.post(endpoint, { query: QUERY_REPO }, githubConfig);
 
 export default async (req, res) => {
 	const { message = 'hello, murph' } = req.query;
@@ -26,13 +24,10 @@ export default async (req, res) => {
 		const { data } = await repoFetched.then(({ data }) => data);
 		const repositoryId = lodash.get(data, 'repository.id');
 		console.log('查询到的仓库ID：', data, repositoryId);
-		const params = { repositoryId, title: 'x', body: 'y' };
-		const creation = await axios.post(githubWebhook, { query: createIssue(params) }, githubConfig);
 		Object.assign(result, {
 			code: 0,
 			payload: {
 				repositoryId,
-				creation: lodash.get(creation, 'data.data')
 			}
 		});
 	} catch(error) {

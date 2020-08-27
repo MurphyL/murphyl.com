93a718c7af769e339d74bcb8147f3e0fa39aa327
murph <murphyl@outlook.com>
2020-08-27 13:36:48 +0800

    改造API - v1.1

--- api/gh_v4.js
+++ api/gh_v4.js
@@ -17,6 +17,7 @@ export default (req, res) => {
 	console.log('地址：', endpoint);
 	console.log('配置：', XHR_CONFIG);
 	axios.post(endpoint, req.body, XHR_CONFIG).then(fetched => {
+		console.log('数据查询参数：', req.body);
 		console.log('数据查询完毕：', fetched);
 		res.json({
 			code: 0,
@@ -25,6 +26,7 @@ export default (req, res) => {
 			}
 		});
 	}).catch(error => {
+		console.log('数据查询参数：', req.body);
 		console.log('数据查询出错：', error);
 		res.json({
 			code: 1,

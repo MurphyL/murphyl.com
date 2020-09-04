f80789c0e4aacabdb32e10e3975de963682fcfae
murph <murphyl@outlook.com>
2020-09-04 12:35:19 +0800

    代码片段适配移动版

---
+++ src/includes/murph_trigger.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const TRIGGER_STYLE = {
+	cursor: 'pointer', 
+};
+
+const LeftIcon = () => (
+	<svg viewBox="0 0 16 16" className="bi bi-text-indent-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+		<path fillRule="evenodd" d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm.646 2.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L4.293 8 2.646 6.354a.5.5 0 0 1 0-.708zM7 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+	</svg>
+);
+
+const RightIcon = () => (
+	<svg viewBox="0 0 16 16" className="bi bi-text-indent-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+		<path fillRule="evenodd" d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm10.646 2.146a.5.5 0 0 1 .708.708L11.707 8l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zM2 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+	</svg>
+);
+
+const DownIcon = () => (
+	<svg viewBox="0 0 16 16" className="bi bi-sort-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M3 2a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-1 0v-10A.5.5 0 0 1 3 2z"/>
+      <path fillRule="evenodd" d="M5.354 10.146a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L3 11.793l1.646-1.647a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 9a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
+    </svg>
+);
+
+
+const MurphTrigger = ({ show, type, onTrigger, children }) => {
+	return (
+		<div className="icon" style={{ ...TRIGGER_STYLE, opacity: (show ? 0.5 : 1) }} onClick={ onTrigger } >
+	        { show ? (
+	            type === 'left' ? <LeftIcon /> : <DownIcon />
+	        ) : (
+	            type === 'right' ? <LeftIcon /> : <RightIcon />
+	        ) }
+        </div>
+	);
+};
+
+export default MurphTrigger;

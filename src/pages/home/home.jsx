4aa7bd4d83c96dd4347f3beef553e94bd5a00809
murph <murphyl@outlook.com>
2020-09-11 15:38:57 +0800

    更新首页

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div style={{ padding: '20vh 0 25vh', textAlign: 'center' }}>
-            <h2>嘿，你好，我是MurphyL！</h2>
+            <h2>你好，我是MurphyL！</h2>
             <p>欢迎来访我的个人主页！</p>
         </div>
     );

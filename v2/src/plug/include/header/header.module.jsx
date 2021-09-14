538fab2e841f09c907fb88264489f6ff96513fca
MurphyL <murphyl@outlook.com>
2021-09-14 19:01:49 +0800

    v2 版本 - 框架

--- v2/src/plug/include/header/header.module.jsx
+++ v2/src/plug/include/header/header.module.jsx
@@ -19,7 +19,7 @@ export default function Header({ container }) {
         <header className={styles.root}>
             <div className={container}>
                 <Link to="/" className={styles.logo}>
-                    <img src="/image/coffee_bean.png" alt="logo" />
+                    <img src={process.env.REACT_APP_LEGO} alt="logo" />
                     <span>{process.env.REACT_APP_TITLE || '未命名站点'}</span>
                 </Link>
                 <nav className={`${styles.navi} ${show}`}>

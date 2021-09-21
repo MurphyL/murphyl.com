fb8585596cd932d971b1a5e75f66386ee2ab0f1b
murph <murphyl@outlook.com>
2021-09-21 20:18:37 +0800

    API

--- site/api/hello.js
+++ site/api/hello.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
 
 const meta = {
-  name: 'murph',
-  email: 'murphyl@outlook.com',
-  func: __filename,
+    name: 'murph',
+    email: 'murphyl@outlook.com',
+    func: __filename,
 };
 
 export default (req, res) => {
-  const { where = '/' } = req.query;
-  fs.stat(where, (err, stats) => {
-    if (err) {
-      res.json(Object.assign(meta, {
-        message: '指定的路径不存在！'
-      }));
-    } else if (stats.isFile()) {
-      res.json(Object.assign(meta, {
-        list: fs.readFileSync(req.query.where || '/', 'utf8')
-      }));
-    } else if (stats.isDirectory()) {
-      res.json(Object.assign(meta, {
-        list: fs.readdirSync(req.query.where || '/', 'utf8')
-      }));
-    } else {
-      res.json(Object.assign(meta, {
-        message: '指定的路径不存在！'
-      }));
-    }
-  });
+    const { where = '/' } = req.query;
+    fs.stat(where, (err, stats) => {
+        if (err) {
+            res.json({
+                ...meta,
+                message: '指定的路径不存在！'
+            });
+        } else if (stats.isFile()) {
+            res.json({
+                ...meta,
+                list: fs.readFileSync(req.query.where || '/', 'utf8')
+            });
+        } else if (stats.isDirectory()) {
+            res.json({
+                ...meta,
+                list: fs.readdirSync(req.query.where || '/', 'utf8')
+            });
+        } else {
+            res.json({
+                ...meta,
+                message: '指定的路径不存在！'
+            });
+        }
+    });
 };

3d4c6381731758cc12f3c8804a74973150114a06
murph <murphyl@outlook.com>
2021-09-21 19:44:57 +0800

    API

--- site/api/hello.js
+++ site/api/hello.js
@@ -1,10 +1,21 @@
 const { readdirSync } = require('fs');
 
+const meta = {
+  name: 'murph',
+  email: 'murphyl@outlook.com',
+  func: __filename,
+};
+
 export default (req, res) => {
-  res.json({ 
-    name: 'murph', 
-    email: 'murphyl@outlook.com',
-    func: __filename,
-    list: readdirSync(req.query.who || '/', 'utf8'),
-  })
+  try {
+    res.json({
+      ...meta,
+      list: readdirSync(req.query.who || '/', 'utf8'),
+    })
+  } catch (e) {
+    res.json({
+      ...meta,
+      message: `读取系统文件出错：${e.message}`
+    });
+  }
 }

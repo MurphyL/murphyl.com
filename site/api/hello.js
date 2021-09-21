85e9f1a60645bfc80c7e0a19a3c219f3ffee9fc8
murph <murphyl@outlook.com>
2021-09-21 18:59:22 +0800

    API

--- site/api/hello.js
+++ site/api/hello.js
@@ -1,10 +1,9 @@
 const { readFileSync } = require('fs');
-const { join } = require('path');
 
 export default (req, res) => {
   res.json({ 
     name: 'murph', 
     email: 'murphyl@outlook.com',
-    files: readFileSync(join(__dirname, 'config', 'ci.yml'), 'utf8')
+    files: readFileSync(join__dirname, 'utf8')
   })
 }

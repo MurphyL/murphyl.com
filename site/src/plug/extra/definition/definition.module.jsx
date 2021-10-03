8a6f01e225c0d6d0437cec5dbfce4b14a2c73df3
murph <murphyl@outlook.com>
2021-10-04 02:53:21 +0800

    demo 页面

---
+++ site/src/plug/extra/definition/definition.module.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styles from './definition.module.css';
+
+console.log(styles);
+
+export const Title = ({ level, children }) => {
+    return React.createElement(level, { className: styles.title }, children);
+}
+
+export const Paragraph = ({ children }) => {
+    if (children && Array.isArray(children)) {
+        if (children[0] && children[0].type === 'img') {
+            return (
+                <p className={styles.image}>{children}</p>
+            )
+        }
+    }
+    return (
+        <p className={styles.paragraph}>{children}</p>
+    );
+};
+
+export const Prepare = ({ children }) => {
+    return (
+        <div className={styles.prepare}>{children}</div>
+    );
+};
---
+++ site/src/plug/extra/markdown_render.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Markdown2JSX from 'markdown-to-jsx';
+
+import { Title, Paragraph, Prepare } from 'plug/extra/definition/definition.module.jsx';
+
+Markdown.displayName = 'Markdown2JSX';
+
+const H2 = (props) => <Title level='h2' {...props} />;
+
+const definedOption = {
+    overrides: {
+        h1: {
+            component: H2
+        },
+        h2: {
+            component: H2
+        },
+        h3: {
+            component: function H3(props) { return <Title level='h3' {...props} /> }
+        },
+        h4: {
+            component: function H3(props) { return <Title level='h4' {...props} /> }
+        },
+        h5: {
+            component: function H3(props) { return <Title level='h5' {...props} /> }
+        },
+        p: {
+            component: Paragraph
+        },
+        pre: {
+            component: Prepare
+        },
+        table: {
+            props: {
+                border: 1,
+                cellSpacing: 0,
+                cellPadding: 0,
+            }
+        }
+    }
+};
+
+export default function Markdown({ content, options }) {
+    return (
+        <Markdown2JSX children={content || ''} options={options || definedOption} />
+    );
+}

0e5d2d655ff111e7ec459a309d41ae191af0be5d
MurphyL <murphyl@outlook.com>
2021-11-30 21:02:24 +0800

    动态组件

---
+++ site/src/plug/dynamic/index.js
@@ -0,0 +1,10 @@
+
+export { DynamicTable } from './table/dynamic-table.module';
+
+export { NaviTabs } from './tabs/dynamic-tabs.module';
+
+export { default as DynamicPage } from './page/dynamic-page.module';
+
+export { default as DynamicList } from './list/dynamic-list.module';
+
+export { default as DynamicLink } from './link/dynamic-link.module';

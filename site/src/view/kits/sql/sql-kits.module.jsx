d55817f501e1cac9cd1ff973eab588345f0ec23e
murph <murphyl@outlook.com>
2021-12-05 02:38:05 +0800

    增加 SQL 格式化工具类

--- site/src/view/kits/sql/sql-kits.module.jsx
+++ site/src/view/kits/sql/sql-kits.module.jsx
@@ -18,18 +18,31 @@ const editorSetting = {
 
 function SQLFormatter() {
     const [value, setValue] = useState('select 1 from dual');
+    const [upper, setUpper] = useState(true);
+    const [indent, setIndent] = useState(2);
     return (
         <div style={{ padding: '3px', height: 'calc(100% - 3px * 2)' }}>
             <Editor {...editorSetting} value={value} onChange={setValue} />
             <DriftToolbar>
-                <select>
-                    <option>mysql</option>
-                </select>
+                <label>
+                    <span>语法：</span>
+                    <select>
+                        <option>mysql</option>
+                    </select>
+                </label>
+                <label>
+                    <span>关键词大写：</span>
+                    <input type="checkbox" checked={upper} onChange={e => setUpper(e.target.checked)} />
+                </label>
+                <label>
+                    <span>缩进：</span>
+                    <input type="number" defaultValue={indent} onChange={e => setIndent(e.target.value)} />
+                </label>
                 <button onClick={() => {
                     setValue(format(value, {
-                        language: 'mysql',
-                        indent: '  ',
-                        uppercase: true, // Defaults to false
+                        language: 'postgresql',
+                        indent: Array(parseInt(indent) + 1).join(' '),
+                        uppercase: upper, // Defaults to false
                         linesBetweenQueries: 2, // Defaults to 1
                     }));
                 }}>格式化</button>

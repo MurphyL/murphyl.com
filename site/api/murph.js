e5890e2cdfcc9f834b39b5030083cea52fc56251
MurphyL <murphyl@outlook.com>
2021-12-11 17:42:11 +0800

    增加 AJAX API

--- site/api/murph.js
+++ site/api/murph.js
@@ -3,7 +3,6 @@ const kindOf = require('kind-of');
 
 export default (req, res) => {
     if (req.query.type === 'ajax') {
-        res.header('X-FROM-MURPH', "yes")
         res.redirect('http://cijian.us');
     } else {
         res.json({
